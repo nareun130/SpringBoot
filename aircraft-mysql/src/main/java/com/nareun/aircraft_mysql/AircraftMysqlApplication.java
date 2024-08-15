27af9cd6ae5735d64d36cd9753ecf39cfc528a2b
김성호 <nareun130@gmail.com>
2024-08-16 01:17:27 +0900

    feat : mysql기반 aircraft 프로젝트

---
+++ aircraft-mysql/src/main/java/com/nareun/aircraft_mysql/AircraftMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.nareun.aircraft_mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AircraftMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AircraftMysqlApplication.class, args);
+	}
+
+}
---
+++ aircraft-mysql/src/test/java/com/nareun/aircraft_mysql/AircraftMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nareun.aircraft_mysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AircraftMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
